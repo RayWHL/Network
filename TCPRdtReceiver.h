1c217c37b541ab9276f4bc63a2c711bedc645a01
alazymonkey <321582424@qq.com>
2018-12-04 19:04:44 +0800

    done

---
+++ TCPRdtReceiver.h
@@ -0,0 +1,20 @@
+#ifndef STOP_WAIT_RDT_RECEIVER_H
+#define STOP_WAIT_RDT_RECEIVER_H
+#include "RdtReceiver.h"
+class TCPRdtReceiver :public RdtReceiver
+{
+private:
+	int expectSequenceNumberRcvd;	//确认报文序号
+	Packet lastAckPkt;				//上次发送的确认报文
+	const int SeqSize;				//分组序号
+public:
+	TCPRdtReceiver();
+	virtual ~TCPRdtReceiver();
+
+public:
+
+	void receive(Packet &packet);	//接收报文，将被NetworkService调用
+};
+
+#endif
+
---
+++ TCPRdtSender.h
@@ -0,0 +1,33 @@
+#ifndef STOP_WAIT_RDT_SENDER_H
+#define STOP_WAIT_RDT_SENDER_H
+#include <vector>
+#include <fstream>
+#include "RdtSender.h"
+class TCPRdtSender :public RdtSender
+{
+private:
+	const int WindowsSize;
+	const int SeqSize;				//分组序号
+	int expectSequenceNumberSend;	// 下一个发送序号 
+	int base;						//基序号
+	bool waitingState;				// 是否处于等待Ack的状态
+
+	int AckNumber;					//快速重传
+	Packet packetWaitingAck;		//已发送并等待Ack的数据包
+
+	std::vector<Packet> PacketWindows;
+	std::fstream file;
+public:
+
+	bool getWaitingState();
+	bool send(Message &message);						//发送应用层下来的Message，由NetworkServiceSimulator调用,如果发送方成功地将Message发送到网络层，返回true;如果因为发送方处于等待正确确认状态而拒绝发送Message，则返回false
+	void receive(Packet &ackPkt);						//接受确认Ack，将被NetworkServiceSimulator调用	
+	void timeoutHandler(int seqNum);					//Timeout handler，将被NetworkServiceSimulator调用
+
+public:
+	TCPRdtSender();
+	virtual ~TCPRdtSender();
+};
+
+#endif
+
